82b6a56d1e66bcd26eacaf1afa5044802af136d5
NCTona <143495151+NCTona@users.noreply.github.com>
2024-06-29 22:48:20 +0700

    deploy

--- admin_page/js/info-student.js
+++ admin_page/js/info-student.js
@@ -1,4 +1,5 @@
-var apiStudent = 'https://localhost:7256/api/UserClass/class/' + grade_view
+// var apiStudent = 'https://localhost:7256/api/UserClass/class/' + grade_view
+var apiStudent = 'http://localhost:3000/student'
 
 fetch(apiStudent)
 
@@ -7,33 +8,34 @@ fetch(apiStudent)
     })
 
     .then(function (student) {
-        var students = student;
-        return student;
+        for (var i = 1; i <= student.length; i++) {
+            console.log(i)
+            var btnStudentId = '#btn-info-' + i
+            var btn_info_student = document.querySelector(btnStudentId)
+            console.log(btn_info_student)
+            var info_student_alert = document.querySelector('#infoStudentModal')
+            var info_student_modal = document.querySelectorAll('.modal-dialog')
+            var info_student_content = document.querySelectorAll('.modal-content')
+            var button_cancel = document.querySelectorAll('.btn-secondary')
+            console.log(button_cancel)
+
+            btn_info_student.onclick = function () {
+                info_student_alert.setAttribute("style", "display: block")
+                setTimeout(function () {
+                    info_student_alert.setAttribute("style", "display: block; opacity: 1; background-image: linear-gradient(rgba(50, 50, 50, 0.5), rgba(50, 50, 50, 0.5));")
+                })
+                info_student_modal[4].setAttribute("style", "transform: translate(0, 0px);")
+            }
+
+            button_cancel[4].onclick = function () {
+                info_student_alert.setAttribute("style", "display: block ;background-image: linear-gradient(rgba(50, 50, 50, 0), rgba(50, 50, 50, 0))")
+                setTimeout(function () {
+                    info_student_alert.setAttribute("style", "display: none")
+                }, 150)
+            }
+        }
+
     })
 
-for(var i=1; i<=students.length; i++){
-    var btnStudentId = '#btn-info-' + i
-    var btn_info_student = document.querySelector('#btn-info-1')
-    var info_student_alert = document.querySelector('#infoStudentModal')
-    var info_student_modal = document.querySelectorAll('.modal-dialog')
-    var info_student_content = document.querySelectorAll('.modal-content')
-    var button_cancel = document.querySelectorAll('.btn-secondary')
-    
-    btn_info_student.onclick = function () {
-        info_student_alert.setAttribute("style", "display: block")
-        setTimeout(function () {
-            info_student_alert.setAttribute("style", "display: block; opacity: 1; background-image: linear-gradient(rgba(50, 50, 50, 0.5), rgba(50, 50, 50, 0.5));")
-        })
-        info_student_modal[4].setAttribute("style", "transform: translate(0, 0px);")
-        info_student_content[4].innerHTML = student[i]
-    }
-    
-    button_cancel[4].onclick = function () {
-        info_student_alert.setAttribute("style", "display: block ;background-image: linear-gradient(rgba(50, 50, 50, 0), rgba(50, 50, 50, 0))")
-        setTimeout(function () {
-            info_student_alert.setAttribute("style", "display: none")
-        }, 150)
-    }
-    
-}
+
 
